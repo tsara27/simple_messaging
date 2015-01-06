0f9b83c3ae9a9a2d80537a0e624af75729672464
tsara27 <tsara.tf@gmail.com>
2015-01-06 20:46:49 +0700

    add respond_with

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,25 @@
 class  MessagesController < ApplicationController
-  def index
-    @messages = Message.all
+  respond_to :html, :js
+  before_action :load_messages
+  before_action :new_message
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
   end
+
+  private
+
+  def load_messages
+    @messages ||= Message.all
+  end
+
+  def new_message
+    @message ||= Message.new
+  end
+
+  def message_params
+    params.require(:message).permit(:name, :text)
+  end
+
 end
